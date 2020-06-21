bbd776ce59c2d279a679e545de5f4e1fb21a46b6
csliujw <695466632@qq.com>
2020-06-21 18:35:55 +0800

    更新题目，二维数组中的查找【普及程序的局部性原理，并讨论堆排序的时间复杂度到底如何】

---
+++ code_interview/src/main/java/com/payphone/sword/FindInArray.java
@@ -0,0 +1,72 @@
+package com.payphone.sword;
+
+/**
+ * @author payphone
+ * @date 2020/6/21 18:06
+ * @Description 二维数组的查找。
+ * <p>
+ * 题目描述
+ * 在一个二维数组中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，
+ * 每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，
+ * 判断数组中是否含有该整数。
+ */
+public class FindInArray {
+    public static void main(String[] args) {
+
+    }
+
+    // 法一：暴力解，直接遍历
+    public boolean Find(int target, int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                if (array[i][j] == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    /**
+     * 法二：根据数组特点，进行查找。
+     * 不能用左上角的和右下角的为初始点。因为那两个点 是有两个方向可以走的，
+     * 不唯一，加大了解题难度！
+     * <p>
+     * 用右上角或左下角为起始点进行查找。
+     * 以右下角为例子。
+     * 大于 number 向左边走。
+     * 小于 number 向上走。
+     * 就这样一直走下去，直到找到元素 或 元素不符合要求
+     */
+    public boolean Find2(int target, int[][] array) {
+        if(array.length==0 || array[0].length==0) return false;
+        int len = array.length;
+        int row = len - 1, col = 0;
+        while (row >= 0 && col < len) {
+            if (array[row][col] > target) {
+                // 当前元素大了 向上走
+                row -= 1;
+            } else if (array[row][col] < target) {
+                // 当前元素笑了 向右走
+                col += 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 看起来思路一很low 思路二很强是吧。
+     * 你去牛客上测试 会发现 简单粗暴的解法速度更快，
+     * 那个看起来很强的速度更慢
+     *
+     * 这或许是数组寻址造成的。 电脑有个高速缓存的概念。每次会把一大片数据加载cache中。
+     * 数组也是如此，例如：一次把这一列的数据都加载进去。发现这列没有我们要找的数据，又要
+     * 把其他的加载进来，这个操作是耗时的。法一需要加载的次数一般是更少的，法二的加载次数一般是
+     * 更多的。所以可能会出现 法一比法二快不少的情况。
+     *
+     * 堆排序也存在这种情况。不符合程序的局部性原理【数据加载进cache】。
+     */
+}

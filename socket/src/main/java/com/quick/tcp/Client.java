612c11146279cb5e7ba2885bbfe92cdd67f31ff7
csliujw <695466632@qq.com>
2020-08-24 13:27:16 +0800

    更新socket TCP案例

---
+++ socket/src/main/java/com/quick/tcp/Client.java
@@ -0,0 +1,64 @@
+package com.quick.tcp;
+
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    public static void main(String[] args) throws Exception {
+        // 初始化socket
+        Socket socket = new Socket();
+        // 设置超时时间
+        socket.setSoTimeout(3000);
+        // 连接本地 端口2000 超时时间3000ms
+        socket.connect(new InetSocketAddress(Inet4Address.getLocalHost(), 2222), 3000);
+        System.out.println("已经向服务器发起了连接");
+        System.out.println("客户端信息：" + socket.getLocalAddress() + ":" + socket.getLocalPort());
+
+        try {
+            // 发送数据
+            todoMsg(socket);
+        } catch (Exception e) {
+            System.out.println("异常关闭");
+        } finally {
+            socket.close();
+            System.out.println("客户端关闭了");
+        }
+    }
+
+    /**
+     * @param client
+     * @throws IOException
+     * @思路 客户端需要发送数据，需要接受服务器端的数据。
+     * 发送数据用输出流，接受数据用输入流
+     * socket提供了两种流对象，一种是 输出流，用于向服务器发送数据
+     * 一种是 输入流，用于接受服务器输入给客户端的数据
+     */
+    public static void todoMsg(Socket client) throws IOException {
+        boolean flag = true;
+        // 我们通过键盘录入 要发送的数据。
+        BufferedReader consoleWriter = new BufferedReader(new InputStreamReader(System.in));
+        // 客户端发送数据 的流对象
+        BufferedWriter clientSend = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+        // 客戶端接收 服务器的反馈
+        BufferedReader serverRead = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+        do {
+            // 从键盘读取要发送的数据
+            String sendMsg = consoleWriter.readLine();
+            clientSend.write(sendMsg);
+//            clientSend.newLine();
+            clientSend.flush();
+
+            String receive = serverRead.readLine();
+            if ("bytes".equalsIgnoreCase(receive)) {
+                flag = false;
+            }
+            System.out.println(receive);
+        } while (flag);
+
+    }
+
+
+}
+
+
---
+++ socket/src/main/java/com/quick/tcp/Server.java
@@ -0,0 +1,70 @@
+package com.quick.tcp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * 先开服务器 再开客户端
+ */
+public class Server {
+    // 服务器只需要开放端口。我们接受到别人发出的数据，也可以知道对方的IP地址
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(2222);
+        System.out.println("服务器准备就绪");
+        System.out.println("服务器信息：" + serverSocket.getInetAddress() + " P:" + serverSocket.getLocalPort());
+
+        // 接收多个客户端发送的数据
+        while (true) {
+            Socket client = serverSocket.accept();
+            ClientHandler clientHandler = new ClientHandler(client);
+            clientHandler.start();
+        }
+
+    }
+
+    private static class ClientHandler extends Thread {
+        private Socket socket;
+        private boolean flag = true;
+
+        public ClientHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("连接过来的客户端信息：" + socket.getInetAddress() + " P:" + socket.getLocalPort());
+            try {
+                // 获得打印流，用于数据输出，服务器回送数据
+                PrintStream socketOutput = new PrintStream(socket.getOutputStream());
+                // 获得输入流，用于接受客户端的数据
+                BufferedReader socketInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                do {
+                    String str = socketInput.readLine();
+                    if ("bytes".equalsIgnoreCase(str)) {
+                        this.flag = false;
+                        socketOutput.println("byes");
+                    } else {
+                        // 打印收到的数据
+                        System.err.println(str + "OK!");
+                        socketOutput.println("回送: " + str.length() + "10086");
+                    }
+                } while (flag);
+                socketInput.close();
+                socketOutput.close();
+            } catch (Exception e) {
+                System.out.println("连接异常断开");
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("客户端已退出");
+            }
+        }
+    }
+}
